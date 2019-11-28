310d2d20685554009395bb117eb3d1ef9eb81b24
smartjinyu <smartjinyu@gmail.com>
2019-11-28 14:18:22 -0800

    merge proj3 starter code

--- SurfStore_RAFT/src/server.py
+++ SurfStore_RAFT/src/server.py
@@ -2,8 +2,8 @@ from socketserver import ThreadingMixIn
 from threading import Lock
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
-
 import hashlib
+import argparse
 
 
 """ Threaded XML-RPC Server """
@@ -74,7 +74,8 @@ def updatefile(filename, version, hashlist):
     return True
 
 
-""" PROJECT 3 APIs below """
+# PROJECT 3 APIs below
+
 
 # Queries whether this metadata store is a leader
 # Note that this call should work even when the server is "crashed"
@@ -102,6 +103,7 @@ def restore():
     return True
 
 
+
 # "IsCrashed" returns the status of this metadata node (crashed or not)
 # This method should always work, even when the node is crashed
 def isCrashed():
@@ -109,10 +111,72 @@ def isCrashed():
     print("IsCrashed()")
     return True
 
+
+# Requests vote from this server to become the leader
+def requestVote(serverid, term):
+    """Requests vote to be the leader"""
+    return True
+
+
+# Updates fileinfomap
+def appendEntries(serverid, term, fileinfomap):
+    """Updates fileinfomap to match that of the leader"""
+    return True
+
+
+def tester_getversion(filename):
+    return fileinfomap[filename][0]
+
+
+# Reads the config file and return host, port and store list of other servers
+def readconfig(config, servernum):
+    """Reads cofig file"""
+    fd = open(config, 'r')
+    l = fd.readline()
+
+    maxnum = int(l.strip().split(' ')[1])
+
+    if servernum >= maxnum or servernum < 0:
+        raise Exception('Server number out of range.')
+
+    d = fd.read()
+    d = d.splitlines()
+
+    for i in range(len(d)):
+        hostport = d[i].strip().split(' ')[1]
+        if i == servernum:
+            host = hostport.split(':')[0]
+            port = int(hostport.split(':')[1])
+
+        else:
+            serverlist.append(hostport)
+
+    return maxnum, host, port
+
+
 if __name__ == "__main__":
     try:
+        parser = argparse.ArgumentParser(description="SurfStore server")
+        parser.add_argument('config', help='path to config file')
+        parser.add_argument('servernum', type=int, help='server number')
+
+        args = parser.parse_args()
+
+        config = args.config
+        servernum = args.servernum
+
+        # server list has list of other servers
+        serverlist = []
+
+        # maxnum is maximum number of servers
+        maxnum, host, port = readconfig(config, servernum)
+
+        hashmap = dict()
+
+        fileinfomap = dict()
+
         print("Attempting to start XML-RPC Server...")
-        server = threadedXMLRPCServer(('localhost', 8080), requestHandler=RequestHandler)
+        server = threadedXMLRPCServer((host, port), requestHandler=RequestHandler)
         server.register_introspection_functions()
         server.register_function(ping,"surfstore.ping")
         server.register_function(getblock,"surfstore.getblock")
@@ -120,11 +184,14 @@ if __name__ == "__main__":
         server.register_function(hasblocks,"surfstore.hasblocks")
         server.register_function(getfileinfomap,"surfstore.getfileinfomap")
         server.register_function(updatefile,"surfstore.updatefile")
-
-        server.register_function(isLeader,"surfstore.isleader")
+        # Project 3 APIs
+        server.register_function(isLeader,"surfstore.isLeader")
         server.register_function(crash,"surfstore.crash")
         server.register_function(restore,"surfstore.restore")
-        server.register_function(isCrashed,"surfstore.iscrashed")
+        server.register_function(isCrashed,"surfstore.isCrashed")
+        server.register_function(requestVote,"surfstore.requestVote")
+        server.register_function(appendEntries,"surfstore.appendEntries")
+        server.register_function(tester_getversion,"surfstore.tester_getversion")
         print("Started successfully.")
         print("Accepting requests. (Halt program to stop.)")
         server.serve_forever()
